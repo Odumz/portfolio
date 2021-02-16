4d35ff11388c652960ba66e09e857a3a696022f5
Odumz <odumz20@gmail.com>
2021-02-16 03:22:38 +0100

    basic page scaffold

---
+++ src/components/home.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="">
+        <div class="tw-p-24 tw-bg-purple-50 tw-mx-auto tw-h-5/6">
+            <!-- <div style="background-image:url(http://www.sonoquilibrium.com/wp-content/uploads/2014/11/3134971.jpg)" class="tw-text-white tw-bg-gray-700 tw-bg-no-repeat tw-bg-center tw-bg-cover" >
+                <div class="tw-absolute tw-w-2/5 tw-inset-0 tw-bg-black tw-opacity-50 tw-h-full tw-flex tw-flex-col">
+
+                    <h1 class="tw-font-bold tw-text-center tw-text-6xl tw-mt-4">I am Oluwashina Odumosu</h1>
+                    <h2 class=" tw-font-extrabold tw-text-center tw-text-6xl tw-mt-4">Software Engineer (Jr.)</h2>
+
+                    <p class="tw-mt-3 tw-pl-5 tw-pr-5 tw-text-center tw-uppercase">
+                        I help create unforgetable experiences
+                    </p>
+
+                    <div class="tw-mt-8 tw-text-center">
+                        <a href="#what" class="tw-bg-green-500 hover:tw-bg-green-700 tw-text-white tw-font-bold tw-text-center tw-py-2 tw-px-4 tw-rounded">Take a look!</a
+                        >
+                    </div>
+
+                </div>
+            </div> -->
+            h
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/socials.vue
@@ -0,0 +1,54 @@
+<template>
+    <div id="socials" class=" socials tw-flex tw-flex-col tw-items-center tw-mx-auto tw-text-primary tw-bg-secondary tw-p-8 tw-pl-12 tw-pr-16">
+        <ul :style="{background: background || '#2A2F32'}" class="tw-flex tw-flex-col tw-mt-36">
+            <li v-for="(link, index) in socials" :key="index"
+                class="tw-list-none tw-px-2 tw-text-sm tw-py-2"
+                >
+                <router-link
+                    :style="{color: linkColor || '#fff'}"
+                    :to="link.path"
+                    @mouseenter="hover = true "
+                    @mouseleave="hover = false "
+                    class="tw-flex tw-flex-row-reverse tw-px-3 tw-py-3 tw-items-center"
+                    >
+                    <p v-if="hover">{{ link.title }}</p>
+                <icons :name=link.icon class=" tw-mr-3" />
+                </router-link>
+                <!-- <hr style="height: 1px; border:none; color:#F97C46; background-color:#F97C46;" /> -->
+            </li>
+            <li class="tw-transform tw-flex tw-flex-col rotate-270 tw-text-sm tw-mt-16 tw-mx-1 tw-my-8">
+                 <hr style="height: 0.7px; border:none; background-color: #F97C46;" />
+            </li>
+        </ul>
+        <p class="tw-transform tw-flex tw-flex-col rotate-270  tw-mt-10">Check me </p> 
+        <!-- <span class="tw-transform tw-flex tw-flex-col rotate-270 tw-text-sm tw-mx-3"><hr style="height: 0.7px; border:none; background-color: #F97C46;" /></span> -->
+    </div>
+</template>
+
+<script>
+import icons from './icons'
+export default {
+    props: ['socials', 'linkColor', 'background', 'hoverBackground'],
+    components: {
+        icons
+    },
+    data() {
+        return {
+            hover: false,
+        }
+    },
+}
+</script>
+
+<style scoped>
+.rotate-270 {
+    transform: rotate(270deg);
+}
+
+@media screen and (max-width: 850px) {
+    .socials {
+        position: relative;
+        padding-left: 50px;
+    }
+}
+</style>
