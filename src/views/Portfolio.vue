5172c95f51141540aaaae9131c5cc1259ae92b72
Odumz <odumz20@gmail.com>
2021-04-08 00:22:18 +0100

    git connected to portfolio

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -14,88 +14,112 @@ import Experience from '../components/experience.vue'
 // import port from '../components/portfolio.vue'
 import Footer from '../components/footer.vue'
 // import gallery from '../components/Gallery.vue'
+
+import { Octokit } from "@octokit/core";
+
 export default {
-    components: { 
-        // gallery,
-        Experience,
-        Footer,
-        // port
-    },
+  components: { 
+      // gallery,
+      Experience,
+      Footer,
+      // port
+  },
+  mounted() {
+    this.getRepos();
+  },
   data: () => ({
-      content: [
-        {
-          text: 'Home',
-          path: '/work/1',
-          icon: 'lorem'
-        },
-        {
-          text: 'About',
-          path: '/work/2',
-          icon: 'about'
-        },
-        {
-          text: 'Portfolio',
-          path: '/work/3',
-          icon: 'portfolio'
-        },
-        {
-          text: 'Services',
-          path: '/work/4',
-          icon: 'services'
-        },
-        {
-          text: 'Contact',
-          path: '/work/5',
-          icon: 'contact'
-        },
-        {
-          text: 'Blog',
-          path: '/work/6',
-          icon: 'blog'
-        }
-      ],
-      experience: [
-        {
-          company: "Enterprise Development Center",
-          start_time: "05-01-2021 09:10:15",
-          working_till_present: true,
-          end_time: "",
-          description: 'This is some lorem ipsum test awesomely short but needs to be long kinda text but I don\'t know why this nepa people will just take light like that. You people are the worst scum ever. Also this is supposed to be something about my experiences while working here.',
-          location: "Lagos"
-        },
-        {
-          company: "Eko Innovation Center",
-          start_time: "?",working_till_present: false,
-          end_time: "",
-          description: 'This is some lorem ipsum test awesomely short but needs to be long kinda text but I don\'t know why this nepa people will just take light like that. You people are the worst scum ever. Also this is supposed to be something about my experiences while working here.',
-          location: "Lagos"
-        },
-        {
-          company: "Econnect Center",
-          start_time: "05-05-2021 09:10:15",
-          working_till_present: false,
-          end_time: "",
-          description: 'This is some lorem ipsum test awesomely short but needs to be long kinda text but I don\'t know why this nepa people will just take light like that. You people are the worst scum ever. Also this is supposed to be something about my experiences while working here.',
-          location: "Lagos"
-        },
-        {
-          company: "Entertainment Factory Center",
-          start_time: "05-01-2020 09:10:15",
-          working_till_present: true,
-          end_time: "",
-          description: 'This is some lorem ipsum test awesomely short but needs to be long kinda text but I don\'t know why this nepa people will just take light like that. You people are the worst scum ever. Also this is supposed to be something about my experiences while working here.',
-          location: "Lagos"
-        },
-        {
-          company: "Enterprise Disease Center",
-          start_time: "?",
-          working_till_present: false,
-          end_time: "",
-          description: 'This is some lorem ipsum test awesomely short but needs to be long kinda text but I don\'t know why this nepa people will just take light like that. You people are the worst scum ever. Also this is supposed to be something about my experiences while working here.',
-          location: "Lagos"
-        },
-      ]
+    content: [
+      {
+        text: 'Home',
+        path: '/work/1',
+        icon: 'lorem'
+      },
+      {
+        text: 'About',
+        path: '/work/2',
+        icon: 'about'
+      },
+      {
+        text: 'Portfolio',
+        path: '/work/3',
+        icon: 'portfolio'
+      },
+      {
+        text: 'Services',
+        path: '/work/4',
+        icon: 'services'
+      },
+      {
+        text: 'Contact',
+        path: '/work/5',
+        icon: 'contact'
+      },
+      {
+        text: 'Blog',
+        path: '/work/6',
+        icon: 'blog'
+      }
+    ],
+    experience: [
+      {
+        company: "Enterprise Development Center",
+        start_time: "05-01-2021 09:10:15",
+        working_till_present: true,
+        end_time: "",
+        description: 'This is some lorem ipsum test awesomely short but needs to be long kinda text but I don\'t know why this nepa people will just take light like that. You people are the worst scum ever. Also this is supposed to be something about my experiences while working here.',
+        location: "Lagos"
+      },
+      {
+        company: "Eko Innovation Center",
+        start_time: "?",working_till_present: false,
+        end_time: "",
+        description: 'This is some lorem ipsum test awesomely short but needs to be long kinda text but I don\'t know why this nepa people will just take light like that. You people are the worst scum ever. Also this is supposed to be something about my experiences while working here.',
+        location: "Lagos"
+      },
+      {
+        company: "Econnect Center",
+        start_time: "05-05-2021 09:10:15",
+        working_till_present: false,
+        end_time: "",
+        description: 'This is some lorem ipsum test awesomely short but needs to be long kinda text but I don\'t know why this nepa people will just take light like that. You people are the worst scum ever. Also this is supposed to be something about my experiences while working here.',
+        location: "Lagos"
+      },
+      {
+        company: "Entertainment Factory Center",
+        start_time: "05-01-2020 09:10:15",
+        working_till_present: true,
+        end_time: "",
+        description: 'This is some lorem ipsum test awesomely short but needs to be long kinda text but I don\'t know why this nepa people will just take light like that. You people are the worst scum ever. Also this is supposed to be something about my experiences while working here.',
+        location: "Lagos"
+      },
+      {
+        company: "Enterprise Disease Center",
+        start_time: "?",
+        working_till_present: false,
+        end_time: "",
+        description: 'This is some lorem ipsum test awesomely short but needs to be long kinda text but I don\'t know why this nepa people will just take light like that. You people are the worst scum ever. Also this is supposed to be something about my experiences while working here.',
+        location: "Lagos"
+      },
+    ],
+    repo: []
   }),
+  methods: {
+    async getRepos () {
+      // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+      const octokit = new Octokit({ auth: `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` });
+
+      const repository = await octokit.request("GET /users/{org}/repos", {
+        org: "Odumz",
+        type: "public",
+      }).then((response) => {
+        this.repo = response;
+      }).catch((error) => {
+        let err = error.response.data ? error.response.data.message : "Something happened";
+        console.log(err)
+      });
+      return repository;
+    }
+  }
 }
 </script>
 
